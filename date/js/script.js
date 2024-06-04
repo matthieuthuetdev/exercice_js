f191df99f950688d30859aa233acafcfd8e10144
matthieuthuetdev_crm <mthuet.pro@gmail.com>
2024-06-04 16:56:59 +0200

    tentative du chengement de couleure dynamique

--- date/js/script.js
+++ date/js/script.js
@@ -17,29 +17,38 @@ function afficherDate() {
 function afficherHeure() {
     let monHeure = new Date();
     let heures = monHeure.getHours();
-    heures= (heures<10)? "0"+heures:heures;
+    heures = (heures < 10) ? "0" + heures : heures;
 
     let minutes = monHeure.getMinutes();
-      minutes= (minutes < 10)? "0"+minutes: minutes;
-    
+    minutes = (minutes < 10) ? "0" + minutes : minutes;
+
 
     let secondes = monHeure.getSeconds();
-     secondes=  (secondes < 10) ? "0"+secondes:secondes;
+    secondes = (secondes < 10) ? "0" + secondes : secondes;
     let chaineheure = heures + ":" + minutes + ":" + secondes;
     return chaineheure;
+}
+function getColor(heureFormater) {
+heureFormater = heureFormater.split(":");
+heures = heureFormater[0]
+minutes = heureFormater[1]
+secondes = heureFormater[2]
+let red = Math.round((24/255)*heures);
+let green = Math.round((60/255)*minutes);
+let blue = Math.round((60/255)*secondes);
+
+return "rgb("+red+" , "+green+" , "+blue+")";
 }
 const btndate = document.querySelector("#btndate");
 btndate.addEventListener("click", function () {
     document.querySelector("#txtdate").value = afficherDate();
 })
 const btnheure = document.querySelector("#btnheure")
-btnheure.addEventListener("click", function (){
+btnheure.addEventListener("click", function () {
     const formHeure = document.querySelector("#txtheure")
     setInterval(() => {
-    formHeure.value = afficherHeure();
-    let red = (24/255)*heures;
-    let green = (60/255)*minutes;
-    let blue = (60/255)*secondes;
-    body.style.backgroundColor = "rgb("+red+","+green+","+blue+")";
+        formHeure.value = afficherHeure();
+        body.style.backgroundColor = getColor(afficherHeure());
+        console.log(getColor(afficherHeure()))
     }, 1000);
 })
