01ed4c3587b59c6448f31a9239ebf53f05351402
matthieuthuetdev <mthuet.pro@gmail.com>
2024-06-05 07:59:36 +0200

    ajout du changement de couleure dynamique

--- date/js/script.js
+++ date/js/script.js
@@ -1,6 +1,6 @@
 const body = document.querySelector("body")
 function afficherDate() {
-    let maDate = new Date()
+    let maDate = new Date();
     let annees = maDate.getFullYear();
     let mois = maDate.getMonth() + 1;
     if (mois < 10) {
@@ -14,6 +14,7 @@ function afficherDate() {
     console.log(date);
     return date;
 }
+
 function afficherHeure() {
     let monHeure = new Date();
     let heures = monHeure.getHours();
@@ -22,33 +23,31 @@ function afficherHeure() {
     let minutes = monHeure.getMinutes();
     minutes = (minutes < 10) ? "0" + minutes : minutes;
 
-
     let secondes = monHeure.getSeconds();
     secondes = (secondes < 10) ? "0" + secondes : secondes;
     let chaineheure = heures + ":" + minutes + ":" + secondes;
     return chaineheure;
 }
-function getColor(heureFormater) {
-heureFormater = heureFormater.split(":");
-heures = heureFormater[0]
-minutes = heureFormater[1]
-secondes = heureFormater[2]
-let red = Math.round((24/255)*heures);
-let green = Math.round((60/255)*minutes);
-let blue = Math.round((60/255)*secondes);
 
-return "rgb("+red+" , "+green+" , "+blue+")";
+function getColor(heureFormater) {
+    let [heures, minutes, secondes] = heureFormater.split(":");
+    let red = Math.round((heures / 23) * 255);
+    let green = Math.round((minutes / 59) * 255);
+    let blue = Math.round((secondes / 59) * 255);
+    return [red, green, blue];
 }
+
 const btndate = document.querySelector("#btndate");
 btndate.addEventListener("click", function () {
     document.querySelector("#txtdate").value = afficherDate();
-})
-const btnheure = document.querySelector("#btnheure")
+});
+
+const btnheure = document.querySelector("#btnheure");
 btnheure.addEventListener("click", function () {
-    const formHeure = document.querySelector("#txtheure")
+    const formHeure = document.querySelector("#txtheure");
     setInterval(() => {
         formHeure.value = afficherHeure();
-        body.style.backgroundColor = getColor(afficherHeure());
-        console.log(getColor(afficherHeure()))
+        let color = getColor(afficherHeure());
+        body.style.backgroundColor = `rgb(${color[0]}, ${color[1]}, ${color[2]})`;
     }, 1000);
-})
+});
