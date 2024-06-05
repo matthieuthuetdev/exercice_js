8efea9589d987f05b1f8276d4f0741da1e2196dd
matthieuthuetdev_crm <mthuet.pro@gmail.com>
2024-06-05 16:50:33 +0200

    début de l'exercice couleure

---
+++ couleure/js/script.js
@@ -0,0 +1,7 @@
+const body = document.querySelector("body");
+const formred = document.querySelector("#formred");
+const btnred = document.querySelector("#btnred");
+const formgreen = document.querySelector("#formgreen");
+const btngreen = document.querySelector("#btngreen");
+const formblue = document.querySelector("#formblue");
+const btnblue = document.querySelector("#btnblue");
