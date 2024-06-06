10c2b14fd8fd8d5955aa75a87afba68beb332c95
matthieuthuetdev_crm <mthuet.pro@gmail.com>
2024-06-06 10:14:10 +0200

    progression de l'exercice couleure

--- couleure/js/script.js
+++ couleure/js/script.js
@@ -5,3 +5,20 @@ const formgreen = document.querySelector("#formgreen");
 const btngreen = document.querySelector("#btngreen");
 const formblue = document.querySelector("#formblue");
 const btnblue = document.querySelector("#btnblue");
+const form = document.querySelectorAll(".form");
+const btn = document.querySelectorAll(".btn");
+function setcolor(red, green, blue) {
+    body.style.backgroundColor = "#" + red + green + blue;
+
+}
+btn.forEach(element => {
+    element.addEventListener("click", () => {
+        const id = element.id;
+        if (id == "btnred") {
+            formred.value = "ff";
+            formgreen.value = "00";
+            formblue.value = "00";
+            setcolor(formred.value, formgreen.value, formblue.value);
+        }
+    })
+})
