4947e715e3d444e0b0ae8dabe60da51aafd784f8
matthieuthuetdev_crm <mthuet.pro@gmail.com>
2024-06-07 09:12:37 +0200

    finalisation de l'exercice couleure valider par franck

--- couleure/js/script.js
+++ couleure/js/script.js
@@ -7,11 +7,12 @@ const formblue = document.querySelector("#formblue");
 const btnblue = document.querySelector("#btnblue");
 const form = document.querySelectorAll(".form");
 const btn = document.querySelectorAll(".btn");
+const regle = /[0-9a-f]{2}/
 function setcolor(red, green, blue) {
     body.style.backgroundColor = "#" + red + green + blue;
 
 }
-// 
+
 btn.forEach(element => {
     element.addEventListener("click", () => {
         const id = element.id;
@@ -35,6 +36,12 @@ btn.forEach(element => {
 })
 form.forEach(champs =>{
     champs.addEventListener("change", ()=>{
+        if(!regle.test(champs.value)){
+            champs.value = ""
+            champs.setAttribute("placeholder","saisie invalide")
+        }else{
+            champs.setAttribute("placeholder","")
+        }
         setcolor(formred.value,formgreen.value,formblue.value)
     })
 })
