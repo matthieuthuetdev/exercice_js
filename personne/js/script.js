3621af3696a6f0ea8a55a338eb0d665b2e62578e
matthieuthuetdev_crm <mthuet.pro@gmail.com>
2024-06-11 16:56:51 +0200

    progression du tableau

--- personne/js/script.js
+++ personne/js/script.js
@@ -1,31 +1,46 @@
 const body = document.querySelector("body");
 const h1 = document.createElement("h1");
-h1.innerText = "liste des personne inscrites"
+h1.textContent = "liste des personnes inscrites";
 const ul = document.createElement("ul");
-function creattable(liste) {
-    const table = document.createElement("table");
-    const tr = document.createElement("tr");
-    const tdNom = document.createElement("td").textContent = "Nom";
-    const tdPrenom = document.createElement("td").textContent = "Prenom";
-    const tdMail = document.createElement("td").textContent = "Mail";
-    table.appendChild(tr);
-    tr.appendChild(tdNom);
-    tr.appendChild(tdPrenom);
-    tr.appendChild(tdMail);
-    body.appendChild(table);
+function creattable(personnes) {
+  const table = document.createElement("table");
+  const tr = document.createElement("tr");
+  table.appendChild(tr);
+  const tdnom = document.createElement("td");
+  tdnom.textContent = "Nom"
+  tr.appendChild(tdnom);
+  const tdprenom = document.createElement("td");
+  tdprenom.textContent = "prenom";
+  tr.appendChild(tdprenom);
+  const tdemail = document.createElement("td");
+  tdemail.textContent = "Email";
+  tr.appendChild(tdemail);
+  personnes.forEach(Element => {
+    const personne = Element.split(" ")
+    const nom = personne[1];
+    const prenom = personne[0];
+    const email = prenom + "." + nom + "@gmail.com"
+    tr = document.createElement("tr");
+  table.appendChild(tr)
+    const tdnom = document.createElement("td");
+    tdnom.textContent = nom;
+    tr.appendChild(tdnom)
+    const tdprenom = document.createElement("td");
+    tdprenom.textContent = prenom;
+    tr.appendChild(tdprenom)
+    const tdemail = document.createElement("td");
+    tdemail.textContent = email;
 
-
-    ;
+  })
+  body.appendChild(table)
 
 }
-const personne = ['Mike Dev', 'John Makenzie', 'Léa Grande'];
-creattable(personne);
-personne.forEach(Element => {
-    const li = document.createElement("li")
-    li.innerText = Element;
-    ul.appendChild(li);
-
-
-})
+const personnes = ["Mike Dev", "John Makenzie", "Léa Grande"];
+personnes.forEach((Element) => {
+  const li = document.createElement("li");
+  li.innerText = Element;
+  ul.appendChild(li);
+});
 body.appendChild(h1);
 body.appendChild(ul);
+creattable(personnes);
