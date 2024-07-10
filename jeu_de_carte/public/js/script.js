49c5c967b9e900cc95b88f2fc385001f578b4ff8
matthieuthuetdev_crm <mthuet.pro@gmail.com>
2024-07-10 16:53:15 +0200

    fetch

--- jeu_de_carte/public/js/script.js
+++ jeu_de_carte/public/js/script.js
@@ -1,6 +1,6 @@
 async function pars_json(url) {
     const reponse = await fetch(url)
     const fichier = reponse.json()
-    console.log(fichier)
+    console.log(fichier.PromiseResult)
 }
-pars_json("https://arfp.github.io/tp/web/javascript/03-cardgame/cardgame.json")
+    pars_json("https://arfp.github.io/tp/web/javascript/03-ardgame/cardgame.json")
