2979fd87df4a4d3a6593f813660ceb7ac166e076
matthieuthuetdev_crm <mthuet.pro@gmail.com>
2024-07-22 08:39:03 +0200

    suppretion du dossier public

---
+++ jeu_de_carte/js/script.js
@@ -0,0 +1,7 @@
+async function pars_json(url) {
+    const promesse = new Promise(reussi,echouer,)
+    const reponse = await fetch(url)
+    return fichier = reponse.json()
+}
+    const reponse = pars_json("https://arfp.github.io/tp/web/javascript/03-cardgame/cardgame.json")
+    console.log(reponse.ok)
--- jeu_de_carte/public/js/script.js
+++
@@ -1,6 +0,0 @@
-async function pars_json(url) {
-    const reponse = await fetch(url)
-    const fichier = reponse.json()
-    console.log(fichier.PromiseResult)
-}
-    pars_json("https://arfp.github.io/tp/web/javascript/03-ardgame/cardgame.json")
